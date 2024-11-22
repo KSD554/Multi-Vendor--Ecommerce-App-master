2033a14234e4d5ff990b9c8cb417902864f04579
Sadok <kouassisadok3@gmail.com>
2024-11-22 15:25:03 +0000

    c

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://mondial-psi.vercel.app/api/v2";
 
-export const backend_url="https://mondial-psi.vercel.app"
+export const backend_url="https://mondial-psi.vercel.app/"
