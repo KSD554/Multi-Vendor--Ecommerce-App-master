16cabe4c84a6bd4eee5a86676e614fe7a0f26a55
Sadok <kouassisadok3@gmail.com>
2024-11-22 15:53:31 +0000

    cc

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Utilisation de createRoot
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+// Initialisation de React avec createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
+// Appel à reportWebVitals pour des métriques de performance
 reportWebVitals();
