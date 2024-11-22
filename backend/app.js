8c29e572bf19f8f03f374215427ba9ba8ffabf1d
Sadok <kouassisadok3@gmail.com>
2024-11-22 15:23:00 +0000

    cc

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000','https://mondial-ci.vercel.app'],
+  origin: 'https://mondial-ci.vercel.app',
   credentials: true
 }));
 
